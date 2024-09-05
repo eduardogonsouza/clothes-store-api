817fe7ded54e56b3616fb62e8a1b7a433ddddedb
eduardogsouz <eduardogoncsouza@gmail.com>
2024-09-05 20:57:49 -0300

    feat: call brands router in "index.ts"

--- index.ts
+++ index.ts
@@ -2,14 +2,15 @@ import express from "express";
 import cors from "cors";
 
 // import marcasRoutes from './routes/marcas'
-// import carrosRoutes from './routes/carros'
+import brandsRouters from "./routes/brands";
+
 const app = express();
 const port = 3004;
 
 app.use(express.json());
 app.use(cors());
 
-// app.use("/marcas", marcasRoutes)
+app.use("/brands", brandsRouters);
 // app.use("/carros", carrosRoutes)
 
 app.get("/", (req, res) => {
--- routes/brands.ts
+++ routes/brands.ts
@@ -2,10 +2,10 @@ import { PrismaClient } from "@prisma/client";
 import { Router } from "express";
 
 const prisma = new PrismaClient();
-
 const router = Router();
 
-router.get("/", async (req, res) => {
+
+router.get("/", async (req_, res) => {
   try {
     const brands = await prisma.clothingBrand.findMany();
     res.status(200).json(brands);
