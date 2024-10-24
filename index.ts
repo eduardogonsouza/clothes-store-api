c3e5fdc1a14929ac4d0998744f8827c8fba11f89
Thiago Nunes Batista <67487679+thiagonunesbatista@users.noreply.github.com>
2024-10-24 00:32:18 -0300

    feat: use user route

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 
 import clothesRouter from "./routes/clothes";
 import brandsRouter from "./routes/brands";
+import usersRouter from "./routes/users";
 
 const app = express();
 const port = 3004;
@@ -10,6 +11,7 @@ const port = 3004;
 app.use(express.json());
 app.use(cors());
 
+app.use("/users", usersRouter);
 app.use("/brands", brandsRouter);
 app.use("/clothes", clothesRouter);
 
